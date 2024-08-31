30da0e4ec6b2294aab248cd01903df8f75e88203
Harrison Cramer <harrisoncramer@gmail.com>
2024-08-30 21:28:29 -0400

    Moved keys into boba

--- help.go
+++ help.go
@@ -20,7 +20,7 @@ func (m HelpModel) Init() tea.Cmd {
 }
 
 func NewHelpModel(keys ...string) HelpModel {
-	kM := NewKeys(keys...)
+	kM := newKeys(keys...)
 	return HelpModel{
 		help: help.New(),
 		keys: kM,
@@ -41,68 +41,7 @@ func (m HelpModel) Update(msg tea.Msg) (HelpModel, tea.Cmd) {
 func (m HelpModel) View() string {
 	base := strings.Builder{}
 	base.WriteString(fmt.Sprintf("\n%s", m.help.View(m.keys)))
-
 	return base.String()
-}
-
-type keyMap struct {
-	Quit   key.Binding
-	Back   key.Binding
-	Select key.Binding
-	Toggle key.Binding
-	Up     key.Binding
-	Down   key.Binding
-	Filter key.Binding
-	Help   key.Binding
-}
-
-func NewKeys(keys ...string) keyMap {
-	var m keyMap
-	// for _, k := range keys {
-	// 	switch k {
-	// 	case m.Back:
-	// 		m.Back = key.NewBinding(
-	// 			key.WithKeys(m.keys.Back),
-	// 			key.WithHelp(m.keys.Back, "back"),
-	// 		)
-	// 	case m.keys.Quit:
-	// 		m.Quit = key.NewBinding(
-	// 			key.WithKeys(m.keys.Quit),
-	// 			key.WithHelp(m.keys.Quit, "quit"),
-	// 		)
-	// 	case m.keys.Help:
-	// 		m.Help = key.NewBinding(
-	// 			key.WithKeys(m.keys.Help),
-	// 			key.WithHelp(m.keys.Help, "help"),
-	// 		)
-	// 	case m.keys.Select:
-	// 		m.Select = key.NewBinding(
-	// 			key.WithKeys(m.keys.Select),
-	// 			key.WithHelp(m.keys.Select, "select/submit"),
-	// 		)
-	// 	case m.keys.Toggle:
-	// 		m.Toggle = key.NewBinding(
-	// 			key.WithKeys(m.keys.Toggle),
-	// 			key.WithHelp(m.keys.Toggle, "toggle"),
-	// 		)
-	// 	case m.keys.Up:
-	// 		m.Up = key.NewBinding(
-	// 			key.WithKeys(m.keys.Up),
-	// 			key.WithHelp(m.keys.Up, "up"),
-	// 		)
-	// 	case m.keys.Down:
-	// 		m.Down = key.NewBinding(
-	// 			key.WithKeys(m.keys.Down),
-	// 			key.WithHelp(m.keys.Down, "down"),
-	// 		)
-	// 	case m.keys.Filter:
-	// 		m.Filter = key.NewBinding(
-	// 			key.WithKeys(m.keys.Filter),
-	// 			key.WithHelp(m.keys.Filter, "filter"),
-	// 		)
-	// 	}
-	// }
-	return m
 }
 
 func (k keyMap) FullHelp() [][]key.Binding {
--- keys.go
+++ keys.go
@@ -1,5 +1,9 @@
 package boba
 
+import "github.com/charmbracelet/bubbles/key"
+
+// The KeyOpts struct contains all possible keys and can be used
+// to load in a set of keys for boba
 type KeyOpts struct {
 	Up     string `mapstructure:"up"`
 	Down   string `mapstructure:"down"`
@@ -9,4 +13,74 @@ type KeyOpts struct {
 	Quit   string `mapstructure:"quit"`
 	Filter string `mapstructure:"filter"`
 	Help   string `mapstructure:"help"`
+}
+
+// Contains a mapping of all keys to their key bindings (bubbletea type)
+type keyMap struct {
+	Quit   key.Binding
+	Back   key.Binding
+	Select key.Binding
+	Toggle key.Binding
+	Up     key.Binding
+	Down   key.Binding
+	Filter key.Binding
+	Help   key.Binding
+}
+
+var bobaKeys KeyOpts
+
+// Sets the keys in the boba components
+func SetKeys(k KeyOpts) {
+	bobaKeys = k
+}
+
+// Creates a key map that's a subset of available keys, for use in different
+// components that only use some of them
+func newKeys(keys ...string) keyMap {
+	var m keyMap
+	for _, k := range keys {
+		switch k {
+		case bobaKeys.Quit:
+			m.Quit = key.NewBinding(
+				key.WithKeys(bobaKeys.Quit),
+				key.WithHelp(bobaKeys.Quit, "quit"),
+			)
+		case bobaKeys.Select:
+			m.Select = key.NewBinding(
+				key.WithKeys(bobaKeys.Select),
+				key.WithHelp(bobaKeys.Select, "select/submit"),
+			)
+		case bobaKeys.Toggle:
+			m.Toggle = key.NewBinding(
+				key.WithKeys(bobaKeys.Toggle),
+				key.WithHelp(bobaKeys.Toggle, "toggle"),
+			)
+		case bobaKeys.Up:
+			m.Up = key.NewBinding(
+				key.WithKeys(bobaKeys.Up),
+				key.WithHelp(bobaKeys.Up, "up"),
+			)
+		case bobaKeys.Down:
+			m.Down = key.NewBinding(
+				key.WithKeys(bobaKeys.Down),
+				key.WithHelp(bobaKeys.Down, "down"),
+			)
+		case bobaKeys.Back:
+			m.Back = key.NewBinding(
+				key.WithKeys(bobaKeys.Back),
+				key.WithHelp(bobaKeys.Back, "back"),
+			)
+		case bobaKeys.Filter:
+			m.Filter = key.NewBinding(
+				key.WithKeys(bobaKeys.Filter),
+				key.WithHelp(bobaKeys.Filter, "filter"),
+			)
+		case bobaKeys.Help:
+			m.Help = key.NewBinding(
+				key.WithKeys(bobaKeys.Help),
+				key.WithHelp(bobaKeys.Help, "help"),
+			)
+		}
+	}
+	return m
 }
